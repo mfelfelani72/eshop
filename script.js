a8a2870196fad75f2cd0ee04daa8374c36e19b31
mfelfelani72@gmail.com <mfcompany21@gmail.com>
2024-04-04 12:37:36 +0400

    mobile menu

---
+++ script.js
@@ -0,0 +1,15 @@
+function copyMenu(){
+
+    var dptCategory = document.querySelector('.dpt-cat');
+    var dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
+
+    var mainNav = document.querySelector('.header-nav nav');
+    var navPlace = document.querySelector('.off-canvas nav');
+    navPlace.innerHTML = mainNav.innerHTML;
+
+    var topNav = document.querySelector('.header-top .wrapper');
+    var topPlace = document.querySelector('.off-canvas .thetop-nav');
+}
+
+copyMenu();
